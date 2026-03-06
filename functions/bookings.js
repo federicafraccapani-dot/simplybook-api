c7bbccd15e1de416d1d3053dd5935c9b28d43e5a
federicafraccapani-dot <federica.fraccapani@gmail.com>
2026-03-06 09:42:06 +0100

    Update bookings.js

--- functions/bookings.js
+++ functions/bookings.js
@@ -6,29 +6,41 @@ export async function onRequestGet() {
 
   // STEP 1 — ottenere token
   const auth = await fetch(
-    "https://user-api-v2.simplybook.it/admin/auth",
+    "https://user-api.simplybook.it/",
     {
       method: "POST",
       headers: {
-        "Content-Type": "application/json",
-        "X-Company-Login": "micemore",
-        "X-API-Key": "047b3e6349938ce1f4b8e84e4b357bb8eb6de3968fcc9a5788d125dbe2c0cf72",
-        "X-API-Secret": "a8ebe7f3c807665fd36c0775cd1edea0c7ea19412a9137b41c5644ddab00dee8"
-      }
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        jsonrpc: "2.0",
+        method: "getToken",
+        params: {
+          apiKey: "047b3e6349938ce1f4b8e84e4b357bb8eb6de3968fcc9a5788d125dbe2c0cf72"
+        },
+        id: 1
+      })
     }
   );
 
   const authData = await auth.json();
-  const token = authData.token;
+  const token = authData.result;
 
   // STEP 2 — recuperare prenotazioni
   const bookings = await fetch(
-    "https://user-api-v2.simplybook.it/admin/bookings",
+    "https://user-api.simplybook.it/",
     {
+      method: "POST",
       headers: {
-        "X-Company-Login": "COMPANY_LOGIN",
+        "Content-Type": "application/json",
         "X-Token": token
-      }
+      },
+      body: JSON.stringify({
+        jsonrpc: "2.0",
+        method: "getBookings",
+        params: {},
+        id: 1
+      })
     }
   );
 
