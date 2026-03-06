b3f6b8755c4b4f8eec21941ba91f0879c52e275c
federicafraccapani-dot <federica.fraccapani@gmail.com>
2026-03-06 12:43:57 +0100

    Update bookings.js

--- functions/bookings.js
+++ functions/bookings.js
@@ -5,56 +5,75 @@
 /*login: "gp@micemorevents.it" */
 /*password: "Micemore2026+" */
 
-export async function onRequestGet() {
+export async function onRequestGet(context) {
 
   try {
+
+    const url = new URL(context.request.url);
+    const clientId = url.searchParams.get("clientId");
+
+    if(!clientId){
+      return new Response(JSON.stringify({
+        error:"missing clientId"
+      }),{headers:{ "Content-Type":"application/json" }});
+    }
+
 
     // STEP 1 — ottenere token
     const login = await fetch(
       "https://user-api.simplybook.me/login",
       {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
+        method:"POST",
+        headers:{
+          "Content-Type":"application/json"
         },
-        body: JSON.stringify({
-          jsonrpc: "2.0",
-          method: "getToken",
-          params: [
+        body:JSON.stringify({
+          jsonrpc:"2.0",
+          method:"getToken",
+          params:[
             "micemore",
             "047b3e6349938ce1f4b8e84e4b357bb8eb6de3968fcc9a5788d125dbe2c0cf72"
           ],
-          id: 1
+          id:1
         })
       }
     );
 
     const loginData = await login.json();
-
-    if(!loginData.result){
-      return new Response(JSON.stringify(loginData),{
-        headers:{ "Content-Type":"application/json" }
-      });
-    }
-
     const token = loginData.result;
 
 
-    // STEP 2 — recuperare prenotazioni
+    // STEP 2 — calcolo sign
+    const encoder = new TextEncoder();
+    const data = encoder.encode(clientId + "047b3e6349938ce1f4b8e84e4b357bb8eb6de3968fcc9a5788d125dbe2c0cf72");
+
+    const hashBuffer = await crypto.subtle.digest("SHA-1", data);
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+    const sign = hashArray.map(b => b.toString(16).padStart(2, "0")).join("");
+
+
+    // STEP 3 — prenotazioni cliente
     const bookings = await fetch(
       "https://user-api.simplybook.me",
       {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          "X-Company-Login": "micemore",
-          "X-Token": token
+        method:"POST",
+        headers:{
+          "Content-Type":"application/json",
+          "X-Company-Login":"micemore",
+          "X-Token":token
         },
-        body: JSON.stringify({
-          jsonrpc: "2.0",
-          method: "getBookings",
-          params: [],
-          id: 2
+        body:JSON.stringify({
+          jsonrpc:"2.0",
+          method:"getClientBookings",
+          params:[
+            clientId,
+            sign,
+            {
+              upcoming_only:false,
+              confirmed_only:false
+            }
+          ],
+          id:2
         })
       }
     );
