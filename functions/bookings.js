387b1c890580e785ba3f16a73624faae1007b570
federicafraccapani-dot <federica.fraccapani@gmail.com>
2026-03-06 11:51:59 +0100

    Update bookings.js

--- functions/bookings.js
+++ functions/bookings.js
@@ -1,15 +1,15 @@
 /* login: micemore */
 /* key: 047b3e6349938ce1f4b8e84e4b357bb8eb6de3968fcc9a5788d125dbe2c0cf72 */
 /* secret: a8ebe7f3c807665fd36c0775cd1edea0c7ea19412a9137b41c5644ddab00dee8 */
-/*company: "micemore",*/
-        /*login: "gp@micemorevents.it",*/
-        /*password: "Micemore2026+"*/
+/*company: "micemore" */
+/*login: "gp@micemorevents.it" */
+/*password: "Micemore2026+" */
 
 export async function onRequestGet() {
 
   try {
 
-    // STEP 1 — login e token
+    // STEP 1 — login
     const login = await fetch(
       "https://user-api.simplybook.me/login",
       {
@@ -18,32 +18,25 @@ export async function onRequestGet() {
           "Content-Type": "application/json"
         },
         body: JSON.stringify({
-          jsonrpc: "2.0",
-          method: "getUserToken",
-          params: [
-            {
-              company: "micemore",
-              login: "gp@micemorevents.it",
-              password: "Micemore2026+"
-            }
-          ],
-          id: 1
+          company: "micemore",
+          login: "gp@micemorevents.it",
+          password: "Micemore2026+"
         })
       }
     );
 
     const loginData = await login.json();
 
-    if(!loginData.result){
-      return new Response(JSON.stringify(loginData), {
+    if(!loginData.token){
+      return new Response(JSON.stringify(loginData),{
         headers:{ "Content-Type":"application/json" }
       });
     }
 
-    const token = loginData.result;
+    const token = loginData.token;
 
 
-    // STEP 2 — recupero prenotazioni
+    // STEP 2 — getBookings
     const bookings = await fetch(
       "https://user-api.simplybook.me/admin/",
       {
@@ -57,35 +50,25 @@ export async function onRequestGet() {
           jsonrpc: "2.0",
           method: "getBookings",
           params: [],
-          id: 2
+          id: 1
         })
       }
     );
 
-    const bookingsData = await bookings.json();
+    const data = await bookings.text();
 
-
-    // STEP 3 — risposta
-    return new Response(
-      JSON.stringify(bookingsData),
-      {
-        headers: {
-          "Content-Type": "application/json",
-          "Access-Control-Allow-Origin": "*"
-        }
+    return new Response(data,{
+      headers:{
+        "Content-Type":"application/json",
+        "Access-Control-Allow-Origin":"*"
       }
-    );
+    });
 
-  } catch (err) {
+  } catch(err){
 
     return new Response(
       JSON.stringify({error:err.toString()}),
-      {
-        headers: {
-          "Content-Type":"application/json",
-          "Access-Control-Allow-Origin":"*"
-        }
-      }
+      {headers:{ "Content-Type":"application/json" }}
     );
 
   }
