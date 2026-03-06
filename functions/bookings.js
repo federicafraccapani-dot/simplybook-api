2a2903d16f3dca181273d7e84ba2b3efbbb62f07
federicafraccapani-dot <federica.fraccapani@gmail.com>
2026-03-06 09:32:56 +0100

    Update bookings.js

--- functions/bookings.js
+++ functions/bookings.js
@@ -1,47 +1,21 @@
+/* login: micemore */
+/* key: 047b3e6349938ce1f4b8e84e4b357bb8eb6de3968fcc9a5788d125dbe2c0cf72 */
+/* secret: a8ebe7f3c807665fd36c0775cd1edea0c7ea19412a9137b41c5644ddab00dee8 */
+
 export async function onRequestGet() {
 
-  // STEP 1: ottenere token
-  const auth = await fetch(
-    "https://user-api.simplybook.it/login",
+  const response = await fetch(
+    "https://user-api-v2.simplybook.it/admin/bookings",
     {
-      method: "POST",
       headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        jsonrpc: "2.0",
-        method: "getToken",
-        params: {
-          apiKey: "047b3e6349938ce1f4b8e84e4b357bb8eb6de3968fcc9a5788d125dbe2c0cf72",
-          secret: "a8ebe7f3c807665fd36c0775cd1edea0c7ea19412a9137b41c5644ddab00dee8"
-        },
-        id: 1
-      })
+        "X-Company-Login": "micemore",
+        "X-API-Key": "047b3e6349938ce1f4b8e84e4b357bb8eb6de3968fcc9a5788d125dbe2c0cf72",
+        "X-API-Secret": "a8ebe7f3c807665fd36c0775cd1edea0c7ea19412a9137b41c5644ddab00dee8"
+      }
     }
   );
 
-  const authData = await auth.json();
-  const token = authData.result;
-
-  // STEP 2: recuperare bookings
-  const bookings = await fetch(
-    "https://user-api.simplybook.it/",
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "X-Token": token
-      },
-      body: JSON.stringify({
-        jsonrpc: "2.0",
-        method: "getBookings",
-        params: {},
-        id: 1
-      })
-    }
-  );
-
-  const data = await bookings.text();
+  const data = await response.text();
 
   return new Response(data, {
     headers: {
